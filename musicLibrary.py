83c0ddec66c4d1c17a919ac6ca6c9047462dcad6
Vishal Mavinmar <vishalmavinmar3@gmail.com>
2025-08-20 23:14:09 +0530

    Delete musicLibrary.py

--- musicLibrary.py
+++
@@ -1,9 +0,0 @@
-music = {
-
-    "perfect": "https://youtu.be/cNGjD0VG4R8?list=RDcNGjD0VG4R8",
-    "shape": "https://youtu.be/JGwWNGJdvx8?list=RDJGwWNGJdvx8",
-    "sapphire":"https://youtu.be/JgDNFQ2RaLQ?list=RDJgDNFQ2RaLQ",
-    "cheapthrills":"https://youtu.be/nYh-n7EOtMA?list=RDnYh-n7EOtMA",
-    "wakawaka" :"https://youtu.be/DUT5rEU6pqM?list=RDDUT5rEU6pqM"
-
-}
